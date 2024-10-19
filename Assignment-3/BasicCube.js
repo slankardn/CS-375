583902c51de0e5117d8aba4b58deecbd2849cc86
slankardn <slankardn@sonoma.edu>
2024-10-19 12:44:36 -0700

    Update BasicCube.js

--- Assignment-3/BasicCube.js
+++ Assignment-3/BasicCube.js
@@ -7,11 +7,154 @@
 
 class BasicCube {
     constructor(gl, vertexShader, fragmentShader) {
+    vertexShader = `
+    		in vec4 aPosition;
+    		in vec4 aColor;
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+    		uniform mat4 P;  	// Projection transformation
+           uniform mat4 MV;	// Model-view transformation
+           const vec4 T = vec4(.5,.5,.5,.5); 	// Translation transformation
+           const vec4 S = vec4(.5,.5,.5,1.0);	// scale transformation
+           
+           out vec4 vColor;
+           
+    		void main() {
+    			vColor = aColor;
+        		gl_Position = P * MV * ((aPosition - T)*S);
+    		}
+	`;
+	fragmentShader = `
+		in vec4 vColor;
+		out vec4 fColor;
+		
+		void main(){
+			//const vec4 red = vec4(1, 0, 0, 1);
+			//const vec4 green = vec4(0, 1, 0, 1);
+			//fColor = gl_FrontFacing ? green: red;
+			fColor = vColor;
+		}
+	`;
+		
+	// I need about 12*3*3 = 108 for cube
+	let positions = new Float32Array([
+		 1.0, 1.0, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            
+            1.0, 1.0, 0.0,
+            1.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            
+            1.0, 1.0, 1.0,
+            0.0, 0.0, 1.0,
+            1.0, 0.0, 1.0,
+            
+            0.0, 0.0, 1.0,
+            1.0, 1.0, 1.0,
+            0.0, 1.0, 1.0,
+            
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 1.0,
+            0.0, 1.0, 1.0,
+            
+            0.0, 0.0, 0.0,
+            0.0, 1.0, 1.0,
+            0.0, 1.0, 0.0,
+            
+            0.0, 1.0, 1.0,
+            1.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+            
+            0.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 0.0,
+            
+            1.0, 0.0, 1.0,
+            1.0, 0.0, 0.0,
+            1.0, 1.0, 1.0,
+            
+            1.0, 1.0, 1.0,
+            1.0, 0.0, 0.0,
+            1.0, 1.0, 0.0,
+            
+            0.0, 0.0, 0.0,
+            1.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            
+            0.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 1.0,
 
-        this.draw = () => {
-            // program.use();
+		]);
+		
+		let colors = new Uint8Array([            
+		 255, 255, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 255, 0.0,
+            
+            255, 255, 0.0,
+            255, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            
+            255, 255, 255,
+            0.0, 0.0, 255,
+            255, 0.0, 255,
+            
+            0.0, 0.0, 255,
+            255, 255, 255,
+            0.0, 255, 255,
+            
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 255,
+            0.0, 255, 255,
+            
+            0.0, 0.0, 0.0,
+            0.0, 255, 255,
+            0.0, 255, 0.0,
+            
+            0.0, 255, 255,
+            255, 255, 0.0,
+            0.0, 255, 0.0,
+            
+            0.0, 255, 255,
+            255, 255, 255,
+            255, 255, 0.0,
+            
+            255, 0.0, 255,
+            255, 0.0, 0.0,
+            255, 255, 255,
+            
+            255, 255, 255,
+            255, 0.0, 0.0,
+            255, 255, 0.0,
+            
+            0.0, 0.0, 0.0,
+            255, 0.0, 255,
+            0.0, 0.0, 255,
+            
+            0.0, 0.0, 0.0,
+            255, 0.0, 0.0,
+            255, 0.0, 255,
+
+    ]);
+		
+	
+	
+	let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+			
+	let aPosition = new Attribute(gl, program, "aPosition", positions, 3, gl.FLOAT);
+	
+	let aColor = new Attribute(gl, program, "aColor", colors, 3, gl.UNSIGNED_BYTE, true);
+	        
+      this.draw = () => {
+            program.use();
+            
+            aPosition.enable();
+            aColor.enable();
+            gl.drawArrays(gl.TRIANGLES, 0, aPosition.count);
+            aColor.disable();
+            aPosition.disable();
         };
     }
 };
+
